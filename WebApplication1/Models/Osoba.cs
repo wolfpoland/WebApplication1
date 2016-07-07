76bcd1f836e10e4a51145ed76ad999c755b8d111
wolfpoland <wolfpoland1@gmail.com>
2016-07-07 22:48:17 +0200

    Przed Testowaniem
    Pora na testy babe

--- WebApplication1/Models/Osoba.cs
+++ WebApplication1/Models/Osoba.cs
@@ -7,5 +7,9 @@ namespace WebApplication1.Models
 {
     public class Osoba
     {
+        public int id { get; set; }
+        public string imie { get; set; }
+        public string  nazwisko { get; set; }
+        
     }
 }
--- WebApplication1/Models/Osoby.cs
+++ WebApplication1/Models/Osoby.cs
@@ -5,7 +5,27 @@ using System.Web;
 
 namespace WebApplication1.Models
 {
-    public class Osoby
+    public class Osoby : IManagment
     {
+        List<Osoba> lista = new List<Osoba>();
+        public void addOsoba(Osoba osoba)
+        {
+            lista.Add(osoba);
+        }
+
+        public Osoba[] getAllOsoba()
+        {
+            return lista.ToArray();
+        }
+
+        public Osoba getPersonById(int id)
+        {
+            return lista.Single(k => k.id == id);
+        }
+
+        public void removeOsoba(Osoba osoba)
+        {
+            lista.Remove(osoba);
+        }
     }
 }
